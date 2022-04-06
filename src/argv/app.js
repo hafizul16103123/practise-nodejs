7923f405d04b87916e44776d50e89fd0feef2353
hafiz <islamhafizul158@gmail.com>
2022-04-06 19:49:30 +0600

    add argv

---
+++ src/argv/app.js
@@ -0,0 +1,32 @@
+// /**
+//  The first element is the full path of the node command.
+//  The second element is the full path of the file being executed.
+//  All the additional arguments are present from the third position going forward.
+//  * 
+//  * 
+//  */
+ 
+// const args = process.argv.slice(2)
+// console.log({args})
+// process.argv.forEach((val, index) => {
+//     console.log(`${index}: ${val}`)
+//   })
+
+/**
+ * 
+ */
+ const x = 1
+ const y = 2
+ const z = 3
+ console.count(
+   'The value of x is ' + x + 
+   ' and has been checked .. how many times?'
+ )
+ console.count(
+   'The value of x is ' + x + 
+   ' and has been checked .. how many times?'
+ )
+ console.count(
+   'The value of y is ' + y + 
+   ' and has been checked .. how many times?'
+ )
