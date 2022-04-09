8180fdad6d5b36bb53f16cef932420a7cd119028
hafiz <islamhafizul158@gmail.com>
2022-04-09 17:04:59 +0600

    eventEmitter

--- src/app.js
+++ src/app.js
@@ -1,58 +1,18 @@
-// //************* syncrinous code */
-// const a = 10;
-// let b = 0;
-// console.log('program start')
+const express = require('express');
+const EventEmitter = require('events')
+const app = express();
+// EventEmitter
+const eventEmitter = new EventEmitter()
 
-// setTimeout(() => {
-//     console.log('logic execute');
-//     b = 20;
-// }, 2000)
-
-// console.log(a+b) //give wrong answer
-// console.log('program complete')
-
-
-
-//************* syncrinous to asyncronous */
-const a = 10;
-let b = 0;
-console.log('program start')
-
-
-const waitedData = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(20)
-        console.log('logic execute')
-    }, 2000)
-}).then((data)=>{
-    console.log(a+data)
-    console.log('program complete')
+eventEmitter.on('start',()=>{
+    console.log('events call')
 })
 
-// // Promise
-// const done = false;
-// //create promise
-// const isWorkDone = () => {
-//     return new Promise((resolve, reject) => {
-//         if (done) {
-//             const data = 'Work is done';
-//             resolve(data)
-//         }
-//         const data = 'Not done yet'
-//         reject(data)
-//     })
-// }
-
-// // calling promise
-// const checkIsWorkDone = () => {
-//     isWorkDone()
-//         .then(ok => { console.log(ok) })
-//         .catch(err => { console.log(err) })
-// }
-// checkIsWorkDone()
-
-// // async/await
-const printName = async()=>{
-    return 'pk'
+function e (){
+    eventEmitter.emit('start')
 }
-printName().then((data)=>{console.log(data)})
+e()
+
+
+
+app.listen(3000)
